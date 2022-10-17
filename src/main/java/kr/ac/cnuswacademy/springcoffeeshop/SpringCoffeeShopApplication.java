af2e8821de7ab4e8d69b7ae9eee2766c1dc1207f
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-10-17 15:05:06 +0900

    initial commit

---
+++ src/main/java/kr/ac/cnuswacademy/springcoffeeshop/SpringCoffeeShopApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.cnuswacademy.springcoffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoffeeShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoffeeShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/cnuswacademy/springcoffeeshop/SpringCoffeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.cnuswacademy.springcoffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoffeeShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
