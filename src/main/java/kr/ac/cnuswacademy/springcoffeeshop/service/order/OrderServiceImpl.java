d715dad3ba611672452d7058c0de2df952e775c2
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-10-20 12:56:47 +0900

    feat : 주문 전체 조회, 주문 주문 id로 조회 기능 추가

---
+++ src/main/java/kr/ac/cnuswacademy/springcoffeeshop/service/order/OrderServiceImpl.java
@@ -0,0 +1,62 @@
+package kr.ac.cnuswacademy.springcoffeeshop.service.order;
+
+import kr.ac.cnuswacademy.springcoffeeshop.dto.order.OrderListResponseDto;
+import kr.ac.cnuswacademy.springcoffeeshop.dto.order.OrderResponseDto;
+import kr.ac.cnuswacademy.springcoffeeshop.dto.order.OrderSaveRequestDto;
+import kr.ac.cnuswacademy.springcoffeeshop.dto.order.OrderUpdateRequestDto;
+import kr.ac.cnuswacademy.springcoffeeshop.dto.orderitem.OrderItemListResponseDto;
+import kr.ac.cnuswacademy.springcoffeeshop.entity.Order;
+import kr.ac.cnuswacademy.springcoffeeshop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService{
+
+    private final OrderRepository orderRepository;
+
+    @Override
+    public List<OrderListResponseDto> findAll() {
+        return orderRepository
+                .findAll()
+                .stream()
+                .map(OrderListResponseDto::new)
+                .toList();
+    }
+
+    @Override
+    public OrderResponseDto findById(Long id) throws IllegalArgumentException {
+        Order order = orderRepository.findById(id).orElseThrow(() -> {
+            throw new IllegalArgumentException("해당 id의 주문이 없습니다");
+        });
+        List<OrderItemListResponseDto> orderItemListResponseDtoList = order
+                .getOrderItems()
+                .stream()
+                .map(OrderItemListResponseDto::new)
+                .toList();
+        return new OrderResponseDto(order, orderItemListResponseDtoList);
+    }
+
+    @Override
+    public OrderResponseDto findByUser(Long id) {
+        return null;
+    }
+
+    @Override
+    public Long save(OrderSaveRequestDto requestDto) {
+        return null;
+    }
+
+    @Override
+    public Long update(Long id, OrderUpdateRequestDto requestDto) {
+        return null;
+    }
+
+    @Override
+    public Long delete(Long id) {
+        return null;
+    }
+}
