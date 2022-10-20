8fe74c8c84d76497399748a3d55dfdcabc9a28ec
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-10-20 19:52:29 +0900

    feat : 상품 dto 구현

---
+++ src/main/java/kr/ac/cnuswacademy/springcoffeeshop/dto/product/ProductSaveRequestDto.java
@@ -0,0 +1,26 @@
+package kr.ac.cnuswacademy.springcoffeeshop.dto.product;
+
+import kr.ac.cnuswacademy.springcoffeeshop.entity.Product;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProductSaveRequestDto {
+    private String name;
+    private int price;
+    private int quantity;
+    private String description;
+
+    public Product toEntity() {
+        return Product
+                .builder()
+                .name(this.name)
+                .price(this.price)
+                .quantity(this.quantity)
+                .description(this.description)
+                .build();
+    }
+}
