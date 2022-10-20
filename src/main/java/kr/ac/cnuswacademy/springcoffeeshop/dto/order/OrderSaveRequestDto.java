7707efe2eeebaa27b5ff2e80f922c27c7911fc01
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-10-20 14:07:48 +0900

    feat : 주문 저장을 위한 dto 클래스 추가

--- src/main/java/kr/ac/cnuswacademy/springcoffeeshop/dto/order/OrderSaveRequestDto.java
+++ src/main/java/kr/ac/cnuswacademy/springcoffeeshop/dto/order/OrderSaveRequestDto.java
@@ -1,4 +1,37 @@
 package kr.ac.cnuswacademy.springcoffeeshop.dto.order;
 
+import kr.ac.cnuswacademy.springcoffeeshop.dto.orderitem.OrderItemSaveRequestDto;
+import kr.ac.cnuswacademy.springcoffeeshop.entity.Order;
+import kr.ac.cnuswacademy.springcoffeeshop.entity.OrderItem;
+import kr.ac.cnuswacademy.springcoffeeshop.entity.OrderStatus;
+import kr.ac.cnuswacademy.springcoffeeshop.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class OrderSaveRequestDto {
+    private String address;
+    private Long userId;
+    private String orderStatus;
+    private List<OrderItemSaveRequestDto> orderItems = new ArrayList<>();
+
+    public Order toEntity() {
+        List<OrderItem> orderItemEntitys = orderItems
+                .stream()
+                .map(OrderItemSaveRequestDto::toEntity)
+                .toList();
+
+        return Order
+                .builder()
+                .address(address)
+                .status(OrderStatus.valueOf(orderStatus))
+                .orderItems(orderItemEntitys)
+                .build();
+    }
 }
