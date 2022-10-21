9cdfc93786ad5a9ce6ce0c0a14c69af7e8e45a0f
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-10-21 17:52:11 +0900

    refactor : dto 클래스에 상품 이름 추가

--- src/main/java/kr/ac/cnuswacademy/springcoffeeshop/dto/orderitem/OrderItemListResponseDto.java
+++ src/main/java/kr/ac/cnuswacademy/springcoffeeshop/dto/orderitem/OrderItemListResponseDto.java
@@ -8,11 +8,13 @@ public class OrderItemListResponseDto {
     private Long id;
     private Long quantity;
     private Long orderId;
+    private String productName;
     private Long productId;
     public OrderItemListResponseDto (OrderItem orderItem) {
         this.id = orderItem.getId();
         this.quantity = orderItem.getQuantity();
         this.orderId = orderItem.getOrder().getId();
         this.productId = orderItem.getOrder().getId();
+        this.productName = orderItem.getProduct().getName();
     }
 }
